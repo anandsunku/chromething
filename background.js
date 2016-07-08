9166a9d71fd539538d6b50354820a09fa8dab0f6
Sunku <asunku@paypalcorp.com>
2016-07-08 10:20:55 +0530

    simple test for save

--- background.js
+++ background.js
@@ -1,20 +1,4 @@
 
-//omnibox
-chrome.omnibox.onInputChanged.addListener(
-
-	function(text, suggest){
-		suggest([
-			{content:"color-divs", description: "make everything red"}
-		]);
-	});
-
-chrome.omnibox.onInputEntered.addListener(
-
-	function(text) 
-	{
-		if(text =="color-divs") colorDivs();
-	}
-);
 
 //listening for an event / one-time requests
 // coming from the popup
@@ -42,15 +26,9 @@ chrome.tabs.onUpdated.addListener(
 
 //todo: listening for an event / long-lived connections
 // coming from devtools
-
-
 // send a message to the content script
 var colorDivs = function() {
 	chrome.tabs.getSelected(null, function(tab){
-
-		console.log("samy am i there");
-		console.log(tab.id);
-		console.log(tab.url);
 
 		chrome.tabs.sendMessage(tab.id, {type: "colors-div", color: "#F00"});
 		//setting a badge
--- content.js
+++ content.js
@@ -3,44 +3,9 @@ chrome.extension.onMessage.addListener(
 		switch(message.type) 
 		{
 			case "colors-div":
-				// var divs = document.querySelectorAll("div");
-				// if(divs.length == 0){
-				// 	alert("there are no divs in the page.");
-				// } else {
-				// 	for (var i=0; i<divs.length; i++) {
-				// 		divs[i].style.backgroundColor = message.color;
-				// 	}
-				// }
-				
-				// var div = document.querySelectorAll
-				
-	
-			    // var btn = document.createElement("BUTTON")
-			    // var t = document.createTextNode("CLICK ME");
-			    // btn.appendChild(t);
-			    // btn.innerHTML = "swamy";
-
-			    // //Appending to DOM 
-			    // document.body.appendChild(btn);
-
-			    // working code starts here
-			    // var googletxt = document.getElementById("hplogo");
-			    // googletxt.style.width = 426;
-			    // googletxt.style.height = 320;
-			    // googletxt.style.backgroundSize = "426px 320px";
-			    // googletxt.style.background = "url(https://lh3.googleusercontent.com/-pLwzY7ohA3I/VMWvNRh72RI/AAAAAAAABFo/XPJVxoBGEeg/w426-h320/1293979034) no-repeat";
-			    //element.style.property
-			    //googletxt.innerHTML = "vinayaka here";
-			    // working code ends here
-
-			    // family photo code starts here
-			    // var googletxt = document.getElementById("lga");
-			    // googletxt.innerHTML = "<h1> Jai vinayaka ithu namma area </h1><img src='https://lh3.googleusercontent.com/-pLwzY7ohA3I/VMWvNRh72RI/AAAAAAAABFo/XPJVxoBGEeg/w426-h320/1293979034'></img>";
-				// family photo code ends here
 		
 				var targetmyframe = document.getElementById("lu_pinned_rhs");
 				console.log("jai sri ram");
-				//targetmyframe.innerHTML =  "<img src='http://yellowpages.sulekha.com/photos/869603/1083444.jpg'></img>";
 
 				var divs = document.querySelectorAll("div");
 				if(divs.length === 0) {
