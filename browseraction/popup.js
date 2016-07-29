9e9de308185bb5878a350bb993c2f88d963595b2
Sunku <asunku@paypalcorp.com>
2016-07-29 17:59:39 +0530

    how message go on ...

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -2,6 +2,9 @@
 
 window.onload = function() {
 	document.getElementById("button").onclick = function() {
+		// message now sent will be received by 
+		//  	content.js 		: chrome.extension.onMessage.addListener(
+		//		background.js	: chrome.extension.onMessage.addListener(
 		chrome.extension.sendMessage({type: "color-divs"});
 
 		chrome.tabs.getSelected(null, function(tab){
